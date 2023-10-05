cc2b174b5df4f06d225d5e8d31322c7dc8a9a1a3
AhadTaghiyev <taghiyev.ahad@gmail.com>
2023-10-05 17:31:19 +0400

    finishedtask

---
+++ c#Intro/Program.cs
@@ -0,0 +1,136 @@
+﻿Console.WriteLine( );
+#region Dataypes
+//byte Num = 160;
+// Num = 10;
+//Console.WriteLine(Num);
+
+//int number = 100;
+//Console.WriteLine(number);
+//double Number = 100.66;
+//Console.WriteLine(Number);
+//char a = 'x';
+//Console.WriteLine( a);
+
+//string Adress = "6534563478sdhkfsdhk.././,.,./456,./775";
+//Console.WriteLine(Adress);
+
+//bool Status = false;
+//Console.WriteLine( Status);
+
+//DateTime Today=DateTime.Now;
+//Console.WriteLine(Today);
+
+//DateTime Today = DateTime.UtcNow.AddHours(4);
+//Console.WriteLine(Today);
+#endregion
+
+#region Opeators
+//int Num1 = 10;
+//int Num2 = 20;
+//Console.WriteLine(Num1+Num2);
+//string Name = "Ramazan";
+//string Surname = "Sirinov";
+//string FullName=Name+" "+Surname;
+//Console.WriteLine( Name + " " + Surname);
+#endregion
+
+#region Condtition
+//Console.WriteLine("1");
+//int str  = 5;
+//int num2 = 6;
+//if (num1 > num2) 
+//{
+//    Console.WriteLine("42");
+//}
+//else
+//{
+//    Console.WriteLine("47");
+//}
+
+//Console.WriteLine("3");
+
+//string Name1 = "Puhan";
+//string Name2 = "Puhan";
+
+//if(Name1 == Name2)
+//{
+//    Console.WriteLine("Beraberdir");
+//}
+//else
+//{
+//    Console.WriteLine("Deyil");
+//}
+
+
+#endregion
+
+#region Array,Object
+//var number = 100;
+//var obj = new
+//{
+//    name = "Zeyneb",
+//    surname = "Rehimli",
+//    SeriNo = 5445354,
+//    IsGraduated = false
+//};
+
+//Console.WriteLine(obj.name+" "+obj.surname);
+
+//int[] Numbers= { 100,1,0,55,22,5};
+//string[] Names = { "fsdfsd","sfsdf","dfsdf"};
+//Console.WriteLine(Names);
+#endregion
+
+#region Loops
+//int i = 0;
+//while (5 > i)
+//{
+//    Console.WriteLine("blablablalab");
+//    i++;
+//}
+
+//do
+//{
+//    Console.WriteLine("blblablablab");
+//    i++;
+
+//} while (5>i);
+
+//1      2      /4
+//for (int i=5;  0<i;   i--)
+//{
+//    //3
+//    Console.WriteLine(i);
+//}
+
+#endregion
+
+#region Try Catch
+//Console.WriteLine(10);
+
+//int[] numbers = { 1, 2, 3 };
+
+//try
+//{
+//    for (int i = 0; numbers.Length >= i; i++)
+//    {
+//        Console.WriteLine(numbers[i]);
+//    }
+//}
+//catch (Exception)
+//{
+//    Console.WriteLine("Bilinmeyen xeta bas verdi");
+//}
+//finally
+//{
+
+//}
+
+//Console.WriteLine(numbers[3]);
+#endregion
+
+
+//Task-1
+
+///dasdasdasd
+
